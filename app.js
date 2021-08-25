8a4cb2ee5daf6bcfefbc8ea9934e052140924943
SAMESQUEJO <champot07@gmail.com>
2021-08-26 05:19:43 +0800

    INITIAL

--- app.js
+++ app.js
@@ -2,7 +2,30 @@ import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js
 
 new Vue({
     el: '#app',
+    // data, function, components
     data: () => ({
-        data: 1
-    })
+        movies: [
+            {
+                title: 'Law School',
+                type: 'series',
+                StreamingPlatform: 'Netflix',
+                episodes: 16,
+                img: 'https://asianwiki.com/images/8/86/Law_School-mp1.jpeg'
+            },
+            {
+                title: 'The Good Doctor',
+                type: 'series',
+                StreamingPlatform: 'Netflix',
+                episodes: 65,
+                img: 'https://images-na.ssl-images-amazon.com/images/I/91xw4nhJ6NL._RI_.jpg'
+            },
+            {
+                title: 'The Upside',
+                type: 'movie',
+                StreamingPlatform: 'Netflix',
+                episodes: 1,
+                img: 'https://m.media-amazon.com/images/M/MV5BNzY3NzYyNjI0N15BMl5BanBnXkFtZTgwNjYzMDc0NjM@._V1_.jpg'
+            },
+        ]
+    }),
 });
